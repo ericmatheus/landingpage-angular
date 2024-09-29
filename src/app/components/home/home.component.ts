a499adac8ee80808f845ea47fc624961c7d48d69
ericmatheus <ericmangabeira@gmail.com>
2024-09-29 17:18:33 -0300

    Desenvolvimento página

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { BtnPrimaryComponent } from '../btn-primary/btn-primary.component';
 import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, NgOptimizedImage],
+  imports: [HeaderComponent, NgOptimizedImage,BtnPrimaryComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
