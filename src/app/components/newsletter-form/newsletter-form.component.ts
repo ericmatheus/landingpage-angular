d2305cea1e8c6bc3811476a6523c56ef5d79a099
ericmatheus <ericmangabeira@gmail.com>
2024-10-15 00:53:45 -0300

    desenvolvimento Landing page

--- src/app/components/newsletter-form/newsletter-form.component.ts
+++ src/app/components/newsletter-form/newsletter-form.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { BtnPrimaryComponent } from '../btn-primary/btn-primary.component';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'newsletter-form',
   standalone: true,
-  imports: [BtnPrimaryComponent],
+  imports: [BtnPrimaryComponent, ReactiveFormsModule],
   templateUrl: './newsletter-form.component.html',
   styleUrl: './newsletter-form.component.scss'
 })
 export class NewsletterFormComponent {
+  newsletterForm!: FormGroup;
+
+  constructor(){
+    this.newsletterForm = new FormGroup({
+      name: new FormControl()
+    });
+  }
 
 }
